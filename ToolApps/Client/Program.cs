1d100266737e78a920986af1dde583f6195a5bf7
Eric Greene <eric@t4d.io>
2020-07-23 14:00:52 -0400

    add exercise 2

--- ToolApps/Client/Program.cs
+++ ToolApps/Client/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using ToolApps.Client.Services;
 
 namespace ToolApps.Client
 {
@@ -18,6 +19,7 @@ namespace ToolApps.Client
       builder.RootComponents.Add<App>("app");
 
       builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+      builder.Services.AddScoped<ColorsDataService>();
 
       await builder.Build().RunAsync();
     }
---
+++ ToolApps/Client/Services/ColorsDataService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToolApps.Shared.Models;
+
+namespace ToolApps.Client.Services
+{
+  public class ColorsDataService
+  {
+    private List<Color> _colors;
+
+    public ColorsDataService()
+    {
+      _colors = new List<Color>();
+      _colors.Add(new Color() { Id = 1, Name = "red", Hexcode = "ff0000" });
+      _colors.Add(new Color() { Id = 2, Name = "green", Hexcode = "00ff00" });
+      _colors.Add(new Color() { Id = 3, Name = "blue", Hexcode = "0000ff" });
+    }
+
+    public IEnumerable<Color> All()
+    {
+      return _colors;
+    }
+  }
+}
